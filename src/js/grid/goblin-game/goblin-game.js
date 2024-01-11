f68e00986eb532c730b2a0aea5901ad6624322b5
WLeeto <leeto4848@gmail.com>
2024-01-11 14:26:56 +0300

    hit/losse count addeded

--- src/js/grid/goblin-game/goblin-game.js
+++ src/js/grid/goblin-game/goblin-game.js
@@ -5,42 +5,73 @@ export class GoblinGame {
         this.grid = new GridFormer(document.querySelector(".game-field")); 
         this.grid.create_game_grid();
 
-        this.misses = 0;
-        this.score = 0;
+        this.hits_number = parseInt(document.querySelector(".hit").textContent)
+        this.misses_number = parseInt(document.querySelector(".miss").textContent)
 
-        this.maxMisses = 5;
+        this.was_hitted = true
+
+        this.scoreToGet = 5;
     }
 
     startGame() {
         this.spawnGoblin();
         setInterval(() => {
-            this.misses++;
             this.spawnGoblin();
           }, 1000);
+    
+        document.querySelector(".game-field").addEventListener('click', this.gameLogic.bind(this))
+    }
+
+    gameLogic(event) {
+      if (event.target.tagName.toLowerCase() == "img") {
+        this.hits_number++;
+        document.querySelector(".hit").textContent = this.hits_number;
+        this.was_hitted = true;
+      } else {
+        this.misses_number++;
+        document.querySelector(".miss").textContent = this.misses_number;
+      }
     }
 
     spawnGoblin() {
-        if (this.misses >= this.maxMisses) {
-            alert('Game over');
-            return;
-        } 
-
+        this.win_lose_logic();
+        this.set_hitted();
+        
         const img = this.grid.createGoblinIcon();
         let currentCell = this.grid.getRandomCell();
       
         currentCell.appendChild(img);
-      
         const removeGoblin = () => {
             if(currentCell.contains(img)) {
               currentCell.removeChild(img);  
             }
           };
-      
         setTimeout(removeGoblin, 1000);
-      
-        currentCell.onclick = () => {
-          this.misses--;
-          removeGoblin();
-        };
+
       }
+
+    set_hitted() {
+      if (!this.was_hitted) {
+        this.misses_number++;
+        document.querySelector(".miss").textContent = this.misses_number;
+      }
+      this.was_hitted = false
+    }
+
+    win_lose_logic() {
+      if (this.hits_number >= this.scoreToGet) {
+        this.set_win()
+      }
+      if (this.misses_number >= this.scoreToGet) {
+        this.set_lose()
+      }
+    }
+
+    set_win() {
+      alert('U WIN!')
+    }
+
+    set_lose() {
+      alert('U LOSE!')
+    }
 }
